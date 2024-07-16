1aa4daba2fcd056288d29615969370fccc42262e
webghost241 <justinwong94926@gmail.com>
2024-07-16 14:24:51 +0200

    updated sigh in and sign up as well as new seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,7 +70,7 @@ Event.create!(
   location: "Madison Square Garden, New York, NY",
   date: Date.new(2024, 8, 15),
   price: 75.00,
-  image_url: "https://source.unsplash.com/800x600/?concert",
+  image_url: "https://images.unsplash.com/photo-1565035010268-a3816f98589a?q=80&w=2788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user1
 )
 
@@ -80,7 +80,7 @@ Event.create!(
   location: "Moscone Center, San Francisco, CA",
   date: Date.new(2024, 9, 22),
   price: 150.00,
-  image_url: "https://source.unsplash.com/800x600/?conference",
+  image_url: "https://images.unsplash.com/photo-1504384764586-bb4cdc1707b0?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user2
 )
 
@@ -90,7 +90,7 @@ Event.create!(
   location: "Art Institute of Chicago, Chicago, IL",
   date: Date.new(2024, 10, 5),
   price: 30.00,
-  image_url: "https://source.unsplash.com/800x600/?festival",
+  image_url: "https://images.unsplash.com/photo-1547826039-bfc35e0f1ea8?q=80&w=2872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user3
 )
 
@@ -100,7 +100,7 @@ Event.create!(
   location: "Santa Monica Beach, Santa Monica, CA",
   date: Date.new(2024, 7, 20),
   price: 20.00,
-  image_url: "https://source.unsplash.com/800x600/?party",
+  image_url: "https://images.unsplash.com/photo-1545960122-23d7d68938a6?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user4
 )
 
@@ -110,7 +110,7 @@ Event.create!(
   location: "Central Park, New York, NY",
   date: Date.new(2024, 8, 10),
   price: 50.00,
-  image_url: "https://source.unsplash.com/800x600/?workshop",
+  image_url: "https://images.unsplash.com/photo-1554080353-a576cf803bda?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user5
 )
 
@@ -120,7 +120,7 @@ Event.create!(
   location: "WeWork, San Francisco, CA",
   date: Date.new(2024, 7, 25),
   price: 15.00,
-  image_url: "https://source.unsplash.com/800x600/?meetup",
+  image_url: "https://images.unsplash.com/photo-1559136555-9303baea8ebd?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user1
 )
 
@@ -130,7 +130,7 @@ Event.create!(
   location: "Hilton Hotel, Miami, FL",
   date: Date.new(2024, 8, 30),
   price: 40.00,
-  image_url: "https://source.unsplash.com/800x600/?seminar",
+  image_url: "https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user2
 )
 
@@ -140,7 +140,7 @@ Event.create!(
   location: "Online",
   date: Date.new(2024, 9, 12),
   price: 25.00,
-  image_url: "https://source.unsplash.com/800x600/?webinar",
+  image_url: "https://images.unsplash.com/photo-1588196749597-9ff075ee6b5b?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user3
 )
 
@@ -150,7 +150,7 @@ Event.create!(
   location: "Los Angeles County Museum of Art, Los Angeles, CA",
   date: Date.new(2024, 10, 18),
   price: 35.00,
-  image_url: "https://source.unsplash.com/800x600/?exhibition",
+  image_url: "https://images.unsplash.com/photo-1563000215-e31a8ddcb2d0?q=80&w=2912&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user4
 )
 
@@ -160,7 +160,7 @@ Event.create!(
   location: "Grand Hyatt, Seattle, WA",
   date: Date.new(2024, 7, 29),
   price: 60.00,
-  image_url: "https://source.unsplash.com/800x600/?competition",
+  image_url: "https://images.unsplash.com/photo-1466637574441-749b8f19452f?q=80&w=2428&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user5
 )
 
@@ -171,7 +171,7 @@ Event.create!(
   location: "New Orleans Jazz & Heritage Festival, New Orleans, LA",
   date: Date.new(2024, 8, 25),
   price: 70.00,
-  image_url: "https://source.unsplash.com/800x600/?jazz",
+  image_url: "https://images.unsplash.com/flagged/photo-1569231290377-072234d3ee57?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user1
 )
 
@@ -181,7 +181,7 @@ Event.create!(
   location: "Boston Convention and Exhibition Center, Boston, MA",
   date: Date.new(2024, 9, 5),
   price: 10.00,
-  image_url: "https://source.unsplash.com/800x600/?bookfair",
+  image_url: "https://images.unsplash.com/photo-1631888720622-4746b895af14?q=80&w=2785&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user2
 )
 
@@ -191,7 +191,7 @@ Event.create!(
   location: "Napa Valley, CA",
   date: Date.new(2024, 10, 1),
   price: 80.00,
-  image_url: "https://source.unsplash.com/800x600/?winetasting",
+  image_url: "https://images.unsplash.com/photo-1598306442928-4d90f32c6866?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user3
 )
 
@@ -201,7 +201,7 @@ Event.create!(
   location: "Grauman's Chinese Theatre, Los Angeles, CA",
   date: Date.new(2024, 7, 23),
   price: 45.00,
-  image_url: "https://source.unsplash.com/800x600/?filmpremiere",
+  image_url: "https://images.unsplash.com/photo-1717915604557-94283edbcc1b?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user4
 )
 
@@ -211,7 +211,7 @@ Event.create!(
   location: "New York Fashion Week, New York, NY",
   date: Date.new(2024, 9, 10),
   price: 100.00,
-  image_url: "https://source.unsplash.com/800x600/?fashionshow",
+  image_url: "https://images.unsplash.com/photo-1568251478083-32c27296c2c2?q=80&w=2886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user5
 )
 
@@ -221,7 +221,7 @@ Event.create!(
   location: "The Comedy Store, Los Angeles, CA",
   date: Date.new(2024, 8, 20),
   price: 30.00,
-  image_url: "https://source.unsplash.com/800x600/?comedy",
+  image_url: "https://images.unsplash.com/photo-1543584756-8f40a802e14f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user1
 )
 
@@ -231,7 +231,7 @@ Event.create!(
   location: "Sedona, AZ",
   date: Date.new(2024, 9, 15),
   price: 120.00,
-  image_url: "https://source.unsplash.com/800x600/?yoga",
+  image_url: "https://images.unsplash.com/photo-1545205597-3d9d02c29597?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user2
 )
 
@@ -241,7 +241,7 @@ Event.create!(
   location: "Central Park, New York, NY",
   date: Date.new(2024, 10, 12),
   price: 50.00,
-  image_url: "https://source.unsplash.com/800x600/?marathon",
+  image_url: "https://images.unsplash.com/photo-1596727362302-b8d891c42ab8?q=80&w=2885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user3
 )
 
@@ -251,7 +251,7 @@ Event.create!(
   location: "Broadway, New York, NY",
   date: Date.new(2024, 7, 18),
   price: 65.00,
-  image_url: "https://source.unsplash.com/800x600/?theater",
+  image_url: "https://images.unsplash.com/photo-1576724196831-c3469e6a67ec?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user4
 )
 
@@ -261,7 +261,7 @@ Event.create!(
   location: "Silicon Valley, CA",
   date: Date.new(2024, 8, 27),
   price: 20.00,
-  image_url: "https://source.unsplash.com/800x600/?sciencefair",
+  image_url: "https://images.unsplash.com/photo-1581092333322-31d2fd38a35e?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   user: user5
 )
 
